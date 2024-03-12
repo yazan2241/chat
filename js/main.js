a1dc8b3d0f709caebed860c45cfbb2e3a9e817fa
Yazan <yazansy097@gmail.com>
2024-03-13 01:50:19 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 const dataElement = document.querySelector('.data');
+const player = document.querySelector('.video-js');
+const chat = document.querySelector('#chat');
+const chatItem = document.querySelector('.chatItem');
+const wrapper = document.querySelector('.videowrapper')
+
 
 let messages = [
-    
+
 ];
 
 let key = "messages";
+
+
 
 window.onload = function () {
     if (localStorage.getItem(key) !== null) {
         messages = JSON.parse(localStorage.getItem(key));
     }
-    console.log(messages);
+    handleFullScreenOverlay();
     init();
 }
 
@@ -26,6 +33,45 @@ function init() {
     });
 
 
+
+
+}
+
+function handleFullScreenOverlay() {
+    video = videojs('my-video');
+
+    let fullScreen = document.querySelector('.vjs-fullscreen-control');
+    let nFullscreen = document.createElement('button');
+    nFullscreen.id = 'nFullscreen';
+    nFullscreen.type = "button";
+    fullScreen.parentNode.insertBefore(nFullscreen, fullScreen.nextSibling);
+    nFullscreen.append(fullScreen);
+    fullScreen.disabled = 'disabled';
+    fullScreen.style.pointerEvents = "none";
+
+
+    document.querySelector('#nFullscreen').addEventListener('click', evt => {
+        toggleFullScreen();
+    });
+}
+
+function toggleFullScreen() {
+    if (document.fullscreenElement == wrapper) {
+        if (wrapper.exitFullscreen) {
+            wrapper.exitFullscreen();
+        } else if (wrapper.mozExitFullScreen) {
+            wrapper.mozExitFullScreen();
+        } else if (wrapper.webkitExitFullscreen) {
+            wrapper.webkitExitFullscreen();
+        }
+    }
+    if (wrapper.requestFullscreen) {
+        wrapper.requestFullscreen();
+    } else if (wrapper.mozRequestFullScreen) {
+        wrapper.mozRequestFullScreen();
+    } else if (wrapper.webkitRequestFullscreen) {
+        wrapper.webkitRequestFullscreen();
+    }
 }
 
 function addMessage() {
@@ -42,3 +88,25 @@ function addMessage() {
 }
 
 
+player.addEventListener('play', evt => {
+    chatItem.classList.remove('hidden');
+
+});
+
+
+player.addEventListener('pause', evt => {
+    chatItem.classList.add('hidden');
+    chat.classList.add('hidden');
+});
+
+
+
+
+
+chatItem.addEventListener('click', evt => {
+
+    if (chat.classList.contains('hidden'))
+        chat.classList.remove('hidden');
+    else
+        chat.classList.add('hidden');
+});
